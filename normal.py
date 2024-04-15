83fa61ff12e5380e2b71971d20bf608502658392
Брижицкий Егор <brizhitskii.ea@phystech.edu>
2024-04-15 22:43:20 +0300

    forth commit

--- normal.py
+++ normal.py
@@ -1,2 +1,11 @@
-#хуй
-print("хуй")
+import matplotlib.pyplot as plt
+import numpy as np
+import show
+
+size = 1000
+
+
+x = np.linspace(0, size, size)
+y = np.random.normal(0, 1, size)
+
+show.my_plot(x, y)
